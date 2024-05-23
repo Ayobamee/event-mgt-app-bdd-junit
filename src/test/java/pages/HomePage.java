313393bb73acc0ae10435e8fdefa17239a9a209d
Ayobami Elutade <aelutade@1wa.org>
2024-05-23 15:51:19 +0100

    added method to verify multiple home page elements

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,10 +3,13 @@ package pages;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.junit.Assert;
 
@@ -14,20 +17,15 @@ public class HomePage {
 
     private static WebDriver driver;
 
-    private By registerLoginBtn = By.cssSelector("a[href=\"https://staging.liveet.co/login\"]");
+    private By logoutBtn = By.xpath("//a[contains(text(), 'Log Out')]");
+    private By createEventBtn = By.xpath("//a[contains(text(), 'Create Event')]");
 
     @SuppressWarnings("static-access")
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // public void clickLoginRegisterButton() {
-
-    // driver.findElement(registerLoginBtn).click();
-
-    // }
-
-    public void verifyHomePageItems() {
+    public void verifyHomePageUrl() {
         // Set up a wait with a specified timeout
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         String expectedUrl = "https://staging.liveet.co/home";
@@ -40,6 +38,31 @@ public class HomePage {
 
         // Assert that the expected URL is the current URL
         Assert.assertEquals("URL does not match expected value", expectedUrl, currentUrl);
+    }
+
+    public void checkElementsVisibility(WebDriver driver, List<By> locators) {
+        for (By locator : locators) {
+            try {
+                WebElement element = driver.findElement(locator);
+                if (element.isDisplayed()) {
+                    System.out.println("Element located by " + locator + " is displayed.");
+                } else {
+                    System.out.println("Element located by " + locator + " is not displayed.");
+                }
+            } catch (NoSuchElementException e) {
+                System.out.println("Element located by " + locator + " is not present in the DOM.");
+            }
+        }
+    }
+
+    public void verifyOtherHomePageElements() {
+        // List of locators to check
+        List<By> locators = List.of(
+                logoutBtn,
+                createEventBtn
+        );
+        checkElementsVisibility(driver, locators);
+
     }
 
     public void closeBrowser() {
--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -39,7 +39,8 @@ public class LoginSteps extends loadApp {
     @Then("I should be redirected to the home page")
     public void i_should_be_redirected_to_the_home_page() {
         HomePage homePage = new HomePage(driver);
-        homePage.verifyHomePageItems();
+        homePage.verifyHomePageUrl();
+        homePage.verifyOtherHomePageElements();
         homePage.closeBrowser();
 
     }
